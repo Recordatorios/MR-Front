c69cb7fb00a579d1f7cedb5a33647d4c42e68670
DinoBudino <zahir_mauricio@outlook.es>
2024-07-07 16:13:52 -0500

    debt modal ts modificado

--- src/app/components/debt-modal/debt-modal.component.ts
+++ src/app/components/debt-modal/debt-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './debt-modal.component.html',
   styleUrls: ['./debt-modal.component.css'],
 })
-export class DebtModalComponent {
+export class DebtModalComponent implements OnInit {
   newDebt = {
     numeroDocumento: '',
     empresa: '',
@@ -19,10 +19,33 @@ export class DebtModalComponent {
     fechaVencimiento: '',
     estado: 'pendiente'  // Estado inicial
   };
+  minDate: string = '';
 
   constructor(private authService: AuthService, public dialogRef: MatDialogRef<DebtModalComponent>) {}
 
+  ngOnInit() {
+    // Establecer la fecha mínima en el formulario como la fecha actual
+    const today = new Date();
+    this.minDate = today.toISOString().split('T')[0];
+  }
+
   registerDebt() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // Asegurarse de que la comparación sea solo de fecha
+    const dueDate = new Date(this.newDebt.fechaVencimiento);
+    dueDate.setHours(0, 0, 0, 0);
+
+    const oneWeekAhead = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+    // Ajustar el estado de la deuda basado en la fecha de vencimiento
+    if (dueDate.getTime() === today.getTime() || (dueDate > today && dueDate <= oneWeekAhead)) {
+      this.newDebt.estado = 'proxima';
+    } else if (dueDate < today) {
+      this.newDebt.estado = 'vencida';
+    } else {
+      this.newDebt.estado = 'pendiente';
+    }
+
     // Transformar datos antes de enviar
     this.newDebt.empresa = this.newDebt.empresa.toUpperCase();
     this.newDebt.montoTotal = parseFloat(this.newDebt.montoTotal.toFixed(2));
