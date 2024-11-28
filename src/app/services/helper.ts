9202e4c13858d4470fb985a7b1daefca27d2e960
VCL <142647591+VCL-tt@users.noreply.github.com>
2024-11-28 16:54:15 -0500

    Update helper.ts

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,3 +1,3 @@
-let baserUrl = 'https://mr-back-autumn-fog-561.fly.dev'
+let baserUrl = 'https://mr-back-holy-hill-7889.fly.dev'
 //local -> let baserUrl = 'http://localhost:8080'
 export default baserUrl;
